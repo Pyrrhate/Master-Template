bf06ad75c687b4f002b051fef4e226f0008181cf
Pyrrhate <guillaume.canva@gmail.com>
2026-02-16 11:12:44 +0100

    Update page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -79,3 +79,19 @@ export default async function Home() {
     </main>
   );
 }
+
+import HeroSection from "@/components/HeroSection";
+import StackSection from "@/components/StackSection";
+import GallerySection from "@/components/GallerySection";
+import FooterSection from "@/components/FooterSection";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <HeroSection />
+      <StackSection />
+      <GallerySection />
+      <FooterSection />
+    </div>
+  );
+};
