967780d880148825a2b2f3a7594356b09b420da5
Pyrrhate <guillaume.canva@gmail.com>
2026-02-17 15:46:49 +0100

    rectifications sur le project id

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
+import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
@@ -10,12 +10,11 @@ const singletonTypes = new Set(['siteSettings'])
 export default defineConfig({
   name: 'default',
   title: 'gcanva-studio',
-
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: "l3lfckoz",
+  dataset: "production",
 
   plugins: [
-    deskTool({
+    structureTool({
       structure: (S) =>
         S.list()
           .title('Contenu')
